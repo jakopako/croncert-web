a222746e5ac6dd9f445ffe2715ef7dece9aab695
jakopako <jakob.dhondt@web.de>
2022-03-14 22:53:45 +0100

    coming soon page

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CrONCERT</h1>
+        <p>
+          Coming soon.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
