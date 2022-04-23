9daaad8c14ed98f2cfa700ec1222362672ae69fd
jakopako <jakob.dhondt@web.de>
2022-04-23 10:38:27 +0200

    small seo changes

--- src/components/ConcertList.tsx
+++ src/components/ConcertList.tsx
@@ -1,40 +1,55 @@
-import React from 'react'
-import { Concert } from '../model'
-import ConcertItem from './ConcertItem'
-import ReactPaginate from 'react-paginate';
-import { NoConcerts } from './NoConcerts';
-import './styles.css'
+import React from "react";
+import { Concert } from "../model";
+import ConcertItem from "./ConcertItem";
+import ReactPaginate from "react-paginate";
+import { NoConcerts } from "./NoConcerts";
+import "./styles.css";
 
 interface Props {
   concerts: Concert[];
   page: number;
   totalPages: number;
-  handlePagination: (selectedItem: { selected: number; }) => void;
+  handlePagination: (selectedItem: { selected: number }) => void;
 }
 
-const ConcertList = ({ concerts, page, totalPages, handlePagination }: Props) => {
+const ConcertList = ({
+  concerts,
+  page,
+  totalPages,
+  handlePagination,
+}: Props) => {
   return (
-    <div className='concertlist__box'>
+    <div className="concertlist__box" data-nosnippet>
       <div>
-        {concerts && concerts.map((concert) => (
-          <ConcertItem key={concert.title + concert.date} title={concert.title} location={concert.location} city={concert.city} comment={concert.comment} url={concert.url} date={concert.date}></ConcertItem>
-        ))}
-        {!concerts && (<NoConcerts/>)}
+        {concerts &&
+          concerts.map((concert) => (
+            <ConcertItem
+              key={concert.title + concert.date}
+              title={concert.title}
+              location={concert.location}
+              city={concert.city}
+              comment={concert.comment}
+              url={concert.url}
+              date={concert.date}
+            ></ConcertItem>
+          ))}
+        {!concerts && <NoConcerts />}
       </div>
-      <div className='pagination'>
+      <div className="pagination">
         {concerts && (
-        <ReactPaginate
-          breakLabel="..."
-          nextLabel=">"
-          onPageChange={handlePagination}
-          pageRangeDisplayed={2}
-          marginPagesDisplayed={1}
-          pageCount={totalPages}
-          previousLabel="<"
-        />)}
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=">"
+            onPageChange={handlePagination}
+            pageRangeDisplayed={2}
+            marginPagesDisplayed={1}
+            pageCount={totalPages}
+            previousLabel="<"
+          />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ConcertList
+export default ConcertList;
