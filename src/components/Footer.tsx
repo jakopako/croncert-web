a7b1b1d11eb6d0bcbba6de6ec46a7f700e7bf609
jakopako <jakob.dhondt@web.de>
2022-06-05 10:52:11 +0200

    improved pagination, datepicker & favicons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
-import logo from './github_pink.png'
+import React from "react";
+import logoWhite from "./github_white.png";
+import logoPink from "./github_pink.png";
 
 const Footer = () => {
   return (
-    <div className='footer__box'>
-        <a href='https://github.com/jakopako/croncert-web' target='_blank' rel='noreferrer noopener'>
-            <img src={logo} alt="Github Logo" width="30" height="30"/>
+    <div className="footer__box">
+      <a
+        href="https://github.com/jakopako/croncert-web"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <img
+          src={logoWhite}
+          alt="Github Logo"
+          width="30"
+          height="30"
+          onMouseOver={(e) => (e.currentTarget.src = logoPink)}
+          onMouseOut={(e) => (e.currentTarget.src = logoWhite)}
+        />
+      </a>
+      <div className="contribute__box">
+        <a
+          href="https://github.com/jakopako/croncert-config"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Contribute
         </a>
-        <div className='contribute__box'><a href='https://github.com/jakopako/croncert-config' target='_blank' rel='noreferrer noopener'>Contribute</a></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
