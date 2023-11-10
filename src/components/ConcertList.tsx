d815e5d4d288bfa6bf8f2254c5ecc7622d50f0f1
jakopako <jakob.dhondt@web.de>
2023-11-10 13:19:07 +0100

    wip

--- src/components/ConcertList.tsx
+++ src/components/ConcertList.tsx
@@ -23,6 +23,36 @@ const ConcertList = ({
 }: Props) => {
   return (
     <div className="concertlist__box">
+      {/* <div
+        style={{
+          backgroundImage: "linear-gradient(#578672,  #7eacb3)",
+          // position: "fixed",
+          width: "100%",
+          height: "100%",
+          zIndex: 1,
+        }}
+      ></div> */}
+      <svg
+        style={{
+          // position: "fixed",
+          opacity: 0.6,
+          width: "100%",
+          height: "100%",
+          zIndex: 1,
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <filter id="noiseFilter">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.65"
+            numOctaves="4"
+            stitchTiles="stitch"
+          />
+        </filter>
+
+        <rect width="100%" height="100%" filter="url(#noiseFilter)" />
+      </svg>
       <div>
         {loading && <LoadingPage />}
         {!loading &&
