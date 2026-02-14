b371e8f55e91bebd5e940212fcaf287153ce7956
Felix Canditt <felix.canditt@gmail.com>
2026-02-14 13:00:50 +0100

    changed border radius in filter boxes

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -68,6 +68,7 @@ const SuggestionsWrapperContainer = styled.div`
 const SuggestionsWrapper = styled.div`
   background: ${SearchBarBackgroundColor};
   border: 1px solid ${BorderColor};
+  border-radius: 15px;
   float: right;
   width: 40%;
   min-width: 140px;
@@ -91,6 +92,7 @@ const SuggestionsList = styled.ul`
       cursor: pointer;
       font-weight: 400;
       border: 1px solid ${BorderColor};
+      border-radius: 15px;
     }
   }
 `;
